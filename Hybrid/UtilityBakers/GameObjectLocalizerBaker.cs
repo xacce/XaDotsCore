c3bccca9e6118201b55947d42f8a78877ea1ae87
Xacce <mmodbrreciever@gmail.com>
2024-10-09 03:25:49 +0300

    add

---
+++ Hybrid/UtilityBakers/GameObjectLocalizerBaker.cs
@@ -0,0 +1,17 @@
+﻿
+#if UNITY_EDITOR && !XACCE_UTILITY_BAKERS_OFF &&  LOCALIZATION_PACKAGE_EXISTS
+using Unity.Entities;
+using UnityEngine.Localization.PropertyVariants;
+
+namespace Core.Hybrid.UtilityBakers
+{
+     public class GameObjectLocalizerBaker : Baker<GameObjectLocalizer>
+    {
+        public override void Bake(GameObjectLocalizer authoring)
+        {
+            var e = GetEntity(TransformUsageFlags.None);
+            AddComponentObject(e, authoring);
+        }
+    }
+}
+#endif
