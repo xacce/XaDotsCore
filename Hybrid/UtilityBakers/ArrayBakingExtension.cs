122e92ba16b360685706b7a264cb04a874e26bfd
Xacce <mmodbrreciever@gmail.com>
2025-03-08 21:57:32 +0300

    ext

---
+++ Hybrid/UtilityBakers/ArrayBakingExtension.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Core.Hybrid.UtilityBakers
+{
+    public static class ArrayBakingExtension
+    {
+        [Conditional("UNITY_EDITOR")]
+        public static void BakeFromArray(this DynamicBuffer<Entity> buffer, IBaker baker, GameObject[] objects, TransformUsageFlags usageFlags = TransformUsageFlags.Dynamic)
+        {
+            buffer.CopyFrom(objects.ToList().ConvertAll(input => baker.GetEntity(input, usageFlags)).ToArray());
+        }
+
+        [Conditional("UNITY_EDITOR")]
+        public static void BakeFromArray<T>(this DynamicBuffer<Entity> buffer, IBaker baker, T[] objects, TransformUsageFlags usageFlags = TransformUsageFlags.Dynamic)
+            where T : MonoBehaviour
+        {
+            buffer.CopyFrom(objects.ToList().ConvertAll(input => baker.GetEntity(input, usageFlags)).ToArray());
+        }
+    }
+}
