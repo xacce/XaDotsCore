e388f34fb049cb2a29b620a311cc98a8b307d24b
Xacce <mmodbrreciever@gmail.com>
2025-03-30 17:23:25 +0300

    awake

---
+++ Runtime/SystemGroups/GameAwakeSystemGroup.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+
+namespace Core.Runtime.SystemGroups
+{
+    [UpdateInGroup(typeof(InitializationSystemGroup), OrderFirst = true)]
+    [UpdateBefore(typeof(BeginInitializationEntityCommandBufferSystem))] //neeeeed twice for groups 
+    public partial class GameAwakeSystemGroup : ComponentSystemGroup
+    {
+    }
+}
