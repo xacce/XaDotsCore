293cabaf15d45765782bcd570aa352a7cdfc795f
Xacce <mmodbrreciever@gmail.com>
2025-04-28 13:59:40 +0300

    more groups

---
+++ Runtime/SystemGroups/GameBlobUpdateSystemGroup.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+
+namespace Core.Runtime.SystemGroups
+{
+    [UpdateInGroup(typeof(SimulationSystemGroup), OrderLast = true)]
+    public partial class GameBlobUpdateSystemGroup : ComponentSystemGroup
+    {
+    }
+}
