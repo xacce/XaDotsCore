1506a086c4b9200b24f136aaf6fe0ab43bf2764a
Xacce <1524920+xacce@users.noreply.github.com>
2025-05-28 15:37:42 +0300

    f

---
+++ Runtime/Collections/BlobArra.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Unity.Entities;
+
+namespace Core.Runtime.Collections
+{
+    public static class BlobArrayUtils
+    {
+        public static bool Contains<T>(ref BlobArray<T> array, T item) where T : unmanaged, IEquatable<T>
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Equals(item))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
