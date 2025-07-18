9809cf4e915eccdc39cc2496e67ed6ad09584d78
Xacce <1524920+xacce@users.noreply.github.com>
2025-07-19 01:56:38 +0300

    Update UnparentAuthoring.cs

--- Hybrid/Authoring/UnparentAuthoring.cs
+++ Hybrid/Authoring/UnparentAuthoring.cs
@@ -1,6 +1,7 @@
 ﻿using Core.Runtime.Collections;
 using Unity.Collections;
 using Unity.Entities;
+using Unity.Mathematics;
 using Unity.Transforms;
 using UnityEngine;
 
@@ -36,14 +37,31 @@ namespace Core.Hybrid.Hybrid.Authoring
             var ecb = new EntityCommandBuffer(Allocator.Temp);
             foreach (var (parent, entity) in SystemAPI.Query<Parent>().WithEntityAccess().WithAll<UnparentBakingTemp>().WithOptions(EntityQueryOptions.IncludePrefab))
             {
-                Debug.Log($"Adslkajsldkjaslkdjaslkdjaslkdjaskld: {parent.Value}");
                 if (!SystemAPI.HasBuffer<Child>(parent.Value) || !SystemAPI.HasBuffer<LinkedEntityGroup>(parent.Value)) continue;
-                Debug.Log($"Adslkajsldkjaslkdjaslkdjaslkdjaskld: {entity}");
                 var child = SystemAPI.GetBuffer<Child>(parent.Value).Reinterpret<Entity>();
 
                 var linked = SystemAPI.GetBuffer<LinkedEntityGroup>(parent.Value).Reinterpret<Entity>();
                 child.Remove(entity);
                 linked.Add(entity);
+                // if (state.EntityManager.HasComponent<LocalTransform>(entity))
+                // {
+                //     ecb.SetComponent(entity,new LocalToWorld()
+                //     {
+                //         Value = state.EntityManager.GetComponentData<LocalTransform>(entity).ToMatrix()
+                //     });
+                // }
+                // else
+                // {
+                //     ecb.SetComponent(entity,new LocalToWorld()
+                //     {
+                //         Value = new LocalTransform()
+                //         {
+                //             Position = float3.zero,
+                //             Rotation = quaternion.identity,
+                //             Scale = 1f,
+                //         }.ToMatrix()
+                //     });
+                // }
                 ecb.RemoveComponent<Parent>(entity);
                 ecb.RemoveComponent<PreviousParent>(entity);
                 ecb.RemoveComponent<UnparentBakingTemp>(entity);
@@ -64,6 +82,25 @@ namespace Core.Hybrid.Hybrid.Authoring
             var ecb = new EntityCommandBuffer(Allocator.Temp);
             foreach (var (parent, entity) in SystemAPI.Query<Parent>().WithEntityAccess().WithAll<UnparentBakingTemp>().WithOptions(EntityQueryOptions.IncludePrefab))
             {
+                // if (state.EntityManager.HasComponent<LocalTransform>(entity))
+                // {
+                //     ecb.SetComponent(entity,new LocalToWorld()
+                //     {
+                //         Value = state.EntityManager.GetComponentData<LocalTransform>(entity).ToMatrix()
+                //     });
+                // }
+                // else
+                // {
+                //     ecb.SetComponent(entity,new LocalToWorld()
+                //     {
+                //         Value = new LocalTransform()
+                //         {
+                //             Position = float3.zero,
+                //             Rotation = quaternion.identity,
+                //             Scale = 1f,
+                //         }.ToMatrix()
+                //     });
+                // }
                 ecb.RemoveComponent<Parent>(entity);
                 ecb.RemoveComponent<PreviousParent>(entity);
             }
