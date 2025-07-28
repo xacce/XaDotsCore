ab32918b836c095fdd06abc8d4668b62083581e6
Xacce <1524920+xacce@users.noreply.github.com>
2025-07-29 01:32:12 +0300

    f

---
+++ Hybrid/ByteToggleAttribute.cs
@@ -0,0 +1,16 @@
+﻿namespace Core.Hybrid.Hybrid
+{
+    using System;
+    using UnityEngine;
+
+    [AttributeUsage(AttributeTargets.Field)]
+    public class ByteToggleAttribute : PropertyAttribute
+    {
+        public string Label;
+
+        public ByteToggleAttribute(string label = null)
+        {
+            Label = label;
+        }
+    }
+}
---
+++ Hybrid/ByteTogglePropertyDrawer.cs
@@ -0,0 +1,35 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine.UIElements;
+
+namespace Core.Hybrid.Hybrid
+{
+    [CustomPropertyDrawer(typeof(ByteToggleAttribute))]
+    public class ByteTogglePropertyDrawer : PropertyDrawer
+    {
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        {
+            // Проверим тип
+            if (property.propertyType != SerializedPropertyType.Integer)
+            {
+                return new Label("ByteToggle can only be used with byte fields.");
+            }
+
+            var toggle = new Toggle();
+
+            var attr = attribute as ByteToggleAttribute;
+            toggle.label = string.IsNullOrEmpty(attr?.Label) ? property.displayName : attr.Label;
+
+            toggle.value = property.intValue != 0;
+
+            toggle.RegisterValueChangedCallback(evt =>
+            {
+                property.intValue = evt.newValue ? 1 : 0;
+                property.serializedObject.ApplyModifiedProperties();
+            });
+
+            return toggle;
+        }
+    }
+}
+#endif
