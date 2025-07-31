d91d812c93e99675e109e68e9a8d8dae8ed796f5
Xacce <1524920+xacce@users.noreply.github.com>
2025-07-31 07:51:47 +0300

    f

---
+++ Hybrid/MinMaxSliderFloat2Attribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Core.Hybrid.Hybrid
+{
+    [AttributeUsage(AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
+    public class MinMaxSliderFloat2Attribute : PropertyAttribute
+    {
+        public float MinLimit;
+        public float MaxLimit;
+
+        public MinMaxSliderFloat2Attribute(float minLimit, float maxLimit)
+        {
+            MinLimit = minLimit;
+            MaxLimit = maxLimit;
+        }
+    }
+}
---
+++ Hybrid/MinMaxSliderFloat2AttributeDrawer.cs
@@ -0,0 +1,69 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Core.Hybrid.Hybrid
+{
+    [CustomPropertyDrawer(typeof(MinMaxSliderFloat2Attribute))]
+    public class MinMaxSliderFloat2AttributeDrawer : PropertyDrawer
+    {
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        {
+
+            var attr = (MinMaxSliderFloat2Attribute)attribute;
+
+            var xProp = property.FindPropertyRelative("x");
+            var yProp = property.FindPropertyRelative("y");
+
+            if (xProp == null || yProp == null)
+                return new Label("float2 must have 'x' and 'y' fields.");
+
+            var container = new VisualElement();
+            container.style.flexDirection = FlexDirection.Column;
+
+            // Заголовок
+            var label = new Label(property.displayName);
+            container.Add(label);
+
+            // Горизонтальный блок: значение min | slider | значение max
+            var sliderRow = new VisualElement();
+            sliderRow.style.flexDirection = FlexDirection.Row;
+            sliderRow.style.alignItems = Align.Center;
+            // sliderRow.style.gap = 4;
+
+            // Текстовые метки значений
+            var minLabel = new Label(xProp.floatValue.ToString("0.###"));
+            minLabel.style.minWidth = 40;
+            var maxLabel = new Label(yProp.floatValue.ToString("0.###"));
+            maxLabel.style.minWidth = 40;
+            maxLabel.style.unityTextAlign = TextAnchor.MiddleRight;
+
+            // Сам слайдер
+            var slider = new MinMaxSlider(attr.MinLimit, attr.MaxLimit,attr.MinLimit, attr.MaxLimit)
+            {
+                value = new Vector2(xProp.floatValue, yProp.floatValue),
+                style = { flexGrow = 1 }
+            };
+
+            // Синхронизация: слайдер → свойства + текст
+            slider.RegisterValueChangedCallback(evt =>
+            {
+                xProp.floatValue = evt.newValue.x;
+                yProp.floatValue = evt.newValue.y;
+                property.serializedObject.ApplyModifiedProperties();
+
+                minLabel.text = evt.newValue.x.ToString("0.###");
+                maxLabel.text = evt.newValue.y.ToString("0.###");
+            });
+
+            sliderRow.Add(minLabel);
+            sliderRow.Add(slider);
+            sliderRow.Add(maxLabel);
+
+            container.Add(sliderRow);
+            return container;
+        }
+    }
+}
+#endif
