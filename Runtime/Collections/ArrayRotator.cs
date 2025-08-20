2093f19f78399ae94f9e83f6187654a8539089ce
Xacce <1524920+xacce@users.noreply.github.com>
2025-08-20 14:29:06 +0300

    Update ArrayRotator.cs

--- Runtime/Collections/ArrayRotator.cs
+++ Runtime/Collections/ArrayRotator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Unity.Collections;
+using Unity.Entities;
 
 namespace Xacce.Core.Collections
 {
@@ -12,7 +13,9 @@ namespace Xacce.Core.Collections
     public class ArrayRotator<TLeft, TRight> where TLeft : unmanaged, IEquatable<TLeft> where TRight : IRotatorLeft<TLeft>
     {
         public delegate void Removed(TRight el);
-        public delegate void Rebind(TLeft left,ref TRight right);
+
+        public delegate void Rebind(TLeft left, ref TRight right);
+
         public delegate TRight Add(TLeft left);
 
         private Removed _onRemoved;
@@ -26,9 +29,14 @@ namespace Xacce.Core.Collections
             _onAdd = onAdd;
         }
 
+        public void Rotate(DynamicBuffer<TLeft> left, List<TRight> right)
+        {
+            Rotate(left.AsNativeArray(), right);
+        }
+
         public void Rotate(NativeArray<TLeft> left, List<TRight> right)
         {
-            var diff= left.Length - right.Count;
+            var diff = left.Length - right.Count;
             var valid = Math.Min(left.Length, right.Count);
             for (int i = 0; i < valid; i++)
             {
@@ -36,7 +44,7 @@ namespace Xacce.Core.Collections
                 var rightEl = right[i];
                 if (!leftEl.Equals(rightEl.left))
                 {
-                    _onRebind(leftEl,ref  rightEl);
+                    _onRebind(leftEl, ref rightEl);
                 }
 
                 right[i] = rightEl;
